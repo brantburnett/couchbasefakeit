aa7df50b49732292a4a6b03f3be9f04bf1039892
Brant Burnett <bburnett@centeredgesoftware.com>
2017-04-03 11:47:00 -0400

    Prevent hang when using memcached buckets

--- configure-node.sh
+++ configure-node.sh
@@ -5,7 +5,7 @@ set -m
 
 if [ ! -e "/node-initialized" ] ; then
 	echo "Initializing Couchbase Server..."
-	
+
 	sleep 5
 
 	# Configure cluster, first request may need more time so retry
@@ -14,7 +14,7 @@ if [ ! -e "/node-initialized" ] ; then
 		curl -Ss http://127.0.0.1:8091/pools/default -d memoryQuota=$CB_DATARAM -d indexMemoryQuota=$CB_INDEXRAM -d ftsMemoryQuota=$CB_SEARCHRAM \
 		&& echo \
 		&& break
-		
+
 		echo "Waiting for Couchbase startup..."
 		sleep 3
 	done
@@ -22,46 +22,46 @@ if [ ! -e "/node-initialized" ] ; then
 	curl -Ss http://127.0.0.1:8091/node/controller/setupServices -d services=${CB_SERVICES//,/%2C} && echo
 	curl -Ss http://127.0.0.1:8091/settings/indexes -d storageMode=$CB_INDEXSTORAGE && echo
 	curl -Ss http://127.0.0.1:8091/settings/web -d port=8091 -d "username=$CB_USERNAME" -d "password=$CB_PASSWORD" && echo
-	
+
 	if [[ $CB_SERVICES == *"n1ql"* ]]; then
 		# Wait for the query service to be up and running
 		for attempt in $(seq 5)
 		do
 			curl -s http://127.0.0.1:8093/admin/ping > /dev/null \
 			&& break
-			
+
 			echo "Waiting for query service..."
 			sleep 1
 		done
 	fi
-	
+
 	# Create buckets defined in /startup/buckets.json
 	echo "Creating buckets..."
-	
+
 	while read bucketSettings
 	do
 		curl -Ss http://127.0.0.1:8091/pools/default/buckets -u "$CB_USERNAME:$CB_PASSWORD" $bucketSettings
 	done < <(cat /startup/buckets.json | jq -r '.[] | to_entries | map(.key + "=" + (.value | tostring)) | @sh "-d " + join(" -d ")')
-	
+
 	# Wait for the buckets to be healthy
-	bucketCount=$(cat /startup/buckets.json | jq -r '.[].name' | wc -l)
+	bucketCount=$(cat /startup/buckets.json | jq -r '.[] | select(.bucketType=="membase") | .name' | wc -l)
 	until [ `curl -Ss http://127.0.0.1:8091/pools/default/buckets -u $CB_USERNAME:$CB_PASSWORD | \
 			 jq -r .[].nodes[].status | grep '^healthy$' | wc -l` -eq $bucketCount ];
 	do
 		echo "Waiting for bucket initialization..."
 		sleep 1
 	done
-	
+
 	# Run fakeit
 	while read bucketName
 	do
 		if [ -e "/startup/$bucketName/models/" ]; then
 			echo "Building data for $bucketName..."
-			
+
 			fakeit couchbase --bucket "$bucketName" "/startup/$bucketName/models"
 		fi
 	done < <(cat /startup/buckets.json | jq -r '.[].name')
-	
+
 	while read bucketName
 	do
 		# Create view design documents
@@ -82,7 +82,7 @@ if [ ! -e "/node-initialized" ] ; then
 
 		if [ -e "/startup/$bucketName/indexes.n1ql" ]; then
 			echo "Building indexes on $bucketName..."
-			
+
 			/opt/couchbase/bin/cbq -e http://127.0.0.1:8093/ -q=true -f="/startup/$bucketName/indexes.n1ql"
 
 			# Wait for index build completion
@@ -96,7 +96,7 @@ if [ ! -e "/node-initialized" ] ; then
 			done
 		fi
 	done < <(cat /startup/buckets.json | jq -r '.[].name')
-	
+
 	# Done
 	echo "Couchbase Server initialized."
 	echo "Initialized `date +"%D %T"`" > /node-initialized
@@ -106,4 +106,3 @@ fi
 
 # Wait for Couchbase Server shutdown
 fg 1
-
